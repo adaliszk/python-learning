2d877d65ec9718f7173af9943461b1218ff97f77
spider <spidertattoos.jw@gmail.com>
2023-01-29 21:16:34 +0000

    feat(download-sorter): add MVP for the move file

---
+++ 01-console-game/ProcGen.py
@@ -0,0 +1,34 @@
+import tcod
+
+
+def main() -> None:
+    screen_width = 80
+    screen_height = 50
+
+    player_x = int(screen_width / 2)
+    player_y = int(screen_height / 2)
+
+    tileset = tcod.tileset.load_tilesheet(
+        "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
+    )
+
+    with tcod.context.new_terminal(
+            screen_width,
+            screen_height,
+            tileset=tileset,
+            title="yet another roguelike",
+            vsync=True,
+    ) as context:
+        root_console = tcod.console.Console(screen_width, screen_height, order="F")
+        while True:
+            root_console.print(x=player_x, y=player_y, string="@")
+
+            context.present(root_console)
+
+            for event in tcod.event.wait():
+                if event.type == "QUIT":
+                    raise SystemExit()
+
+
+if __name__ == "__main__":
+    main()
---
+++ 01-console-game/input_handlers.py
@@ -0,0 +1,13 @@
+from typing import Optional
+
+import tcod.event
+
+from actions import Action
+
+
+class EventHandler(tcod.event.EventDispatch[Action]):
+    def ev_quit(self, event: tcod.event.Quit) -> Optional[Action]:
+        raise SystemExit()
+
+    def ev_keydown(self, event: tcod.event.KeyDown) -> Optional[Action]:
+        action: Optional[Action] = None
