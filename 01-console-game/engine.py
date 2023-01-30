7050f1dcc277d0bf477a0afc323e5c744f4e612b
spider <spidertattoos.jw@gmail.com>
2023-01-30 00:11:13 +0000

    feat(download-sorter): add MVP for the move file

--- 01-console-game/engine.py
+++ 01-console-game/engine.py
@@ -3,7 +3,6 @@ from typing import Iterable
 from tcod.console import Console
 from tcod.context import Context
 
-from actions import EscapeAction, MovementAction
 from entity import Entity
 from game_map import GameMap
 from input_handlers import EventHandler
@@ -23,15 +22,10 @@ class Engine:
             if action is None:
                 continue
 
-            if isinstance(action, MovementAction):
-                if self.game_map.tiles["walkable"][self.player.x + action.dx, self.player.y + action.dy]:
-                    self.player.move(dx=action.dx, dy=action.dy)
-
-            elif isinstance(action, EscapeAction):
-                raise SystemExit
+            action.perform(self, self.player)
 
     def render(self, console: Console, context: Context) -> None:
-        self.game_map.render(Console)
+        self.game_map.render(console)
         for entity in self.entities:
             console.print(entity.x, entity.y, entity.char, fg=entity.color)
 
